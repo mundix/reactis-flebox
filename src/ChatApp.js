c6e937f34e3313ef13891e25aea2c2a51417d77e
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-12-30 22:29:42 -0400

    Fixed badge number aligned vertical and horizontal

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import './ChatApp.css';
 
 export const ChatApp = () => {
@@ -10,11 +10,16 @@ export const ChatApp = () => {
                <div className="col-4 chat-rooms">
                   <div className="room d-flex">
                      <div className='col-3 d-flex justify-content-center align-items-center'>
-                        <i className="fas fa-user"></i>
+                        <i className="bi bi-alarm-fill text-warning" style={{ fontSize: 50 }}></i>
                      </div>
-                     <div className='d-flex flex-column'>
-                        <div>Jamal Fabian</div>
-                        <div>15/55/2038</div>
+                     <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <p className='m-0'>Jamal Fabian</p>
+                        <small>15/55/2038</small>
+                     </div>
+                     <div className='col-3 d-flex justify-content-center align-items-center'>
+                           <span className="badge rounded-pill bg-primary ">
+                              99
+                           </span>
                      </div>
 
                   </div>
