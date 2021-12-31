27bd549e2dd9df3a0f2472beac0e9ccd04639552
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-12-31 03:11:50 -0400

    ...

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -17,18 +17,21 @@ export const ChatApp = () => {
    }, []);
 
    const getImageFromApi = async () => {
-      console.log('paso 1')
-       await axios.get(url)
-         .then( (resp) => {
+      console.log('paso 1');
+      await axios.get(url)
+         .then((resp) => {
             setImages(handleGetImageFromState(resp.data.data));
+            // Aqui tampoco esta imprimiendo. 
+            console.log(images);
          });
-         //Aqui no esta imprimiendo lo que guarda con el setImage
-         //Pero en el React Component del inspector estan asignado los states de images
-         console.log(images); 
+      //Aqui no esta imprimiendo lo que guarda con el setImage
+      //Pero en el React Component del inspector estan asignado los states de images
+      console.log('Paso 2');
+      console.log(images);
    }
 
-   const handleGetImageFromState = (images) => {
-      return images.map( image => {
+   const handleGetImageFromState = (arrImages) => {
+      return arrImages.map(image => {
          return image.images.original.url;
       });
    }
@@ -38,21 +41,27 @@ export const ChatApp = () => {
          <div className="container">
             <div className="row">
                <div className="col-4 chat-rooms">
-                  <div className="room d-flex">
-                     <div className='col-3 d-flex justify-content-center align-items-center'>
+                  {
+                     images.length && (images.map(image => {
+                        <div className="room d-flex">
+                           <div className='col-3 d-flex justify-content-center align-items-center'>
 
-                     </div>
-                     <div className='d-flex flex-column justify-content-center align-items-center'>
-                        <p className='m-0'>Jamal Fabian</p>
-                        <small>15/55/2038</small>
-                     </div>
-                     <div className='col-3 d-flex justify-content-center align-items-center'>
-                        <span className="badge rounded-pill bg-primary ">
-                           99
-                        </span>
-                     </div>
+                           </div>
+                           <div className='d-flex flex-column justify-content-center align-items-center'>
+                              <p className='m-0'>Jamal Fabian</p>
+                              <small>15/55/2038</small>
+                           </div>
+                           <div className='col-3 d-flex justify-content-center align-items-center'>
+                              <span className="badge rounded-pill bg-primary ">
+                                 99
+                              </span>
+                           </div>
 
-                  </div>
+                        </div>
+                     })
+                     )
+                  }
+
                </div>
             </div>
          </div>
