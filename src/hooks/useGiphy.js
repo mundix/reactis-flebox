4dc7e01beb6ce4fe66497c5a3b9c9a483f6df277
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-01 06:58:20 -0400

    Created and settle useGiphy custom hook
    this hook will retrieve an array of url from end point

---
+++ src/hooks/useGiphy.js
@@ -0,0 +1,26 @@
+// useFetch Hook retrieve an array of URL from call to a end point on giphy.com 
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { GetImageFromState } from '../helpers/GetImageFromState';
+
+export const useGiphy = () => {
+
+   const apiKey = "Nr4bfrgRK7obHM3O68YwvQ2iUmuxi8Yj";
+   const url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=10`;
+
+   const [images, setImages] = useState([]);
+
+  const getImageFromApi = useCallback(async () => {
+    await axios.get(url)
+      .then((resp) => {
+         setImages(GetImageFromState(resp.data.data));
+      })
+      .catch( err => console.log(err));
+  }, [url]);
+
+  useEffect(()=> {
+      getImageFromApi();
+  }, []);
+
+  return  images;
+}
