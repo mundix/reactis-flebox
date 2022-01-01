c97c149a2a6f204e96ec055a93a283493d8312b2
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-01 10:07:58 -0400

    Updated and fixed axios callback to end point

--- src/hooks/useGiphy.js
+++ src/hooks/useGiphy.js
@@ -1,8 +1,7 @@
 // useFetch Hook retrieve an array of URL from call to a end point on giphy.com 
 import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
+import React, {  useEffect, useState } from "react";
 import { GetImageFromState } from '../helpers/GetImageFromState';
-import { useFetch } from "./useFetch";
 
 export const useGiphy =  () => {
 
@@ -10,17 +9,17 @@ export const useGiphy =  () => {
    const url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=10`;
 
    const [images, setImages] = useState([]);
-   const {data, loading, error} = useFetch(url);
+   const [loading, setLoading] = useState(true);
 
   useEffect(()=> {
-    if(data?.data.length) {
-      setImages(GetImageFromState(data?.data));
-    }
-    // setImages();
-  }, [data]);
+    axios.get(url).then(resp => {
+      setImages(GetImageFromState(resp?.data.data));
+      setLoading(false);
+    })
+  }, [url]);
 
   return  {
     images,
-    loading
+    loading,
   };
 }
