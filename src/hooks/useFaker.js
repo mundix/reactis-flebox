be723fe15ec41b643ce4493d54daacdd9a70a43e
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-01 08:24:38 -0400

    Refactored useFaker hook updated logic to generate a collection

---
+++ src/hooks/useFaker.js
@@ -0,0 +1,29 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import faker from 'faker';
+
+export const useFaker = (images = []) => {
+
+   const [collection, setCollection] = useState([]);
+
+   const generateUserCollection = useCallback(
+      () => {
+         const users = images?.map(image => {
+            return {
+               name: faker?.name.findName(),
+               image: image,
+               date: new Date().toDateString()
+            }
+         });
+         setCollection(users);
+      },
+      [images],
+   )
+   
+   useEffect(() => {
+      generateUserCollection();
+   }, [generateUserCollection]);
+
+   return {
+      collection
+   }
+}
