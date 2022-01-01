1f507a3d2b8fc1d2e1dc37332866283c9520ff77
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-01 07:00:37 -0400

    Refactored ChatApp
    cleaning code and implemente useGiphy custom hook

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,48 +1,29 @@
 import React, { useCallback, useEffect, useState } from "react";
 import "./ChatApp.css";
-import axios from "axios";
 import { RoomItem } from "./components/RoomItem";
+import { useGiphy } from "./hooks/useGiphy";
 
-const apiKey = "Nr4bfrgRK7obHM3O68YwvQ2iUmuxi8Yj";
-const url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=10`;
 
 export const ChatApp = () => {
-  const [images, setImages] = useState([]);
-
-  const getImageFromApi = useCallback(async () => {
-    console.log("paso 1");
-    await axios.get(url).then((resp) => {
-      setImages(handleGetImageFromState(resp.data.data));
-      // Aqui tampoco esta imprimiendo.
-    });
-    //Aqui no esta imprimiendo lo que guarda con el setImage
-    //Pero en el React Component del inspector estan asignado los states de images
-    console.log("Paso 2");
-  }, []);
+  
+  const {images} = useGiphy();
 
   useEffect(() => {
-    getImageFromApi();
+    // getImageFromApi();
   }, []);
 
-  const handleGetImageFromState = (arrImages) => {
-    return arrImages.map((image) => {
-      return image.images.original.url;
-    });
-  };
-
-  console.log(images);
 
   return (
     <>
       <div className="container">
         <div className="row">
-          <div className="col-3 chat-rooms">
+          <div className="col-12 col-md-4 chat-rooms">
             {
-              images && (images.map(image => (
-                <RoomItem image={image}/>
-              ))
-              )
+              images && (images.map(image => <RoomItem image={image}/>)) 
             }
+
+          </div>
+          <div className="col-md-8 chat-body d-none d-sm-block">
 
           </div>
         </div>
