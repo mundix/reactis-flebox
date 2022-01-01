545e74e42f3fc452af11d0d412cb8d16de786616
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-01 07:00:04 -0400

    Created and Updated RoomItem Component
    add col sizes to responsive views

--- src/components/RoomItem.js
+++ src/components/RoomItem.js
@@ -4,19 +4,19 @@ export const RoomItem = ({image}) => {
    return (
       <div className="room d-flex align-items-center my-2">
                   <div
-                    className="col-3"
+                    className="col-4 col-sm-3 col-md-3"
                     style={{
-                      minHeight: "50px",
+                      minHeight: "100px",
                       height: "auto",
                       backgroundImage:
                         `url(${image})`,
                       backgroundRepeat: "no-repeat",
                       backgroundSize: "cover",
                     }}></div>
-                  <div className="col-9 d-flex align-items-center justify-content-between">
-                    <div className="flex-column justify-content-center align-items-center p-1">
+                  <div className="col-8 col-sm-9 col-md-9 d-flex align-items-center justify-content-between">
+                    <div className="flex-column justify-content-center align-items-center p-3 p-md-3">
                       <p className="m-0">Jamal Fabian</p>
-                      <small>15/55/2038</small>
+                      <small clas>15/55/2038</small>
                     </div>
                     <div className="justify-content-center align-items-center mx-2">
                       <span className="badge rounded-pill bg-primary ">
