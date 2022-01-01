0c9c17848bcce227e74302fb1cffeaf837278ce8
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-01 08:25:42 -0400

    Updated argument with user objet and settle on the html

--- src/components/RoomItem.js
+++ src/components/RoomItem.js
@@ -1,30 +1,30 @@
 import React from 'react';
 
-export const RoomItem = ({image}) => {
+export const RoomItem = ({ user }) => {
    return (
       <div className="room d-flex align-items-center my-2">
-                  <div
-                    className="col-4 col-sm-3 col-md-3"
-                    style={{
-                      minHeight: "100px",
-                      height: "auto",
-                      backgroundImage:
-                        `url(${image})`,
-                      backgroundRepeat: "no-repeat",
-                      backgroundSize: "cover",
-                    }}></div>
-                  <div className="col-8 col-sm-9 col-md-9 d-flex align-items-center justify-content-between">
-                    <div className="flex-column justify-content-center align-items-center p-3 p-md-3">
-                      <p className="m-0">Jamal Fabian</p>
-                      <small clas>15/55/2038</small>
-                    </div>
-                    <div className="justify-content-center align-items-center mx-2">
-                      <span className="badge rounded-pill bg-primary ">
-                        99
-                        <i className="bi bi-bar-chart" style={{ fontSize: 10 }}></i>
-                      </span>
-                    </div>
-                  </div>
-                </div>
+         <div
+            className="col-4 col-sm-3 col-md-3"
+            style={{
+               minHeight: "100px",
+               height: "auto",
+               backgroundImage:
+                  `url(${user?.image})`,
+               backgroundRepeat: "no-repeat",
+               backgroundSize: "cover",
+            }}></div>
+         <div className="col-8 col-sm-9 col-md-9 d-flex align-items-center justify-content-between">
+            <div className="flex-column justify-content-center align-items-center p-3 p-md-3">
+               <p className="m-0">{user.name}</p>
+               <small clas>{user.date}</small>
+            </div>
+            <div className="justify-content-center align-items-center mx-2">
+               <span className="badge rounded-pill bg-primary ">
+                  99
+                  <i className="bi bi-bar-chart" style={{ fontSize: 10 }}></i>
+               </span>
+            </div>
+         </div>
+      </div>
    )
 }
